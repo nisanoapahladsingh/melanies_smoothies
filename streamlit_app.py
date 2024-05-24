682bea459e82b5bf75a45fda0b9637041d1b374e
nisanoapahladsingh <nisa.pahladsingh@accenture.com>
2024-05-24 12:52:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,5 +46,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '  +  name_on_order, icon="✅")
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
